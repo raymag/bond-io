312caa71e9a759772af1ec03439a4aa15f94784c
raymag <prayk11@outlook.com>
2018-10-27 17:01:38 -0300

    FEATURE: del_posta - Desenvolvida

---
+++ del_post.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include "inc/functions/connection.php";
+if(!isset($_SESSION["id_user"])){
+  header("location:index.php");
+}
+if(!isset($_GET["p"]) || !isset($_GET["l"])){
+    header("location:home.php");
+}else{
+    $conn = connect();
+    $id_user = $_SESSION["id_user"];
+    $id_post = $_GET["p"];
+    $local = $_GET["l"];
+    $sql = "SELECT * FROM posts WHERE id_post = $id_post AND user = $id_user";
+    if($query = mysqli_query($conn, $sql)){
+        $data = mysqli_fetch_assoc($query);
+        if(isset($data["user"])){
+            $sql = "DELETE FROM likes WHERE post = $id_post";
+            mysqli_query($conn, $sql);
+            $sql = "DELETE FROM posts WHERE user = $id_user AND id_post = $id_post";
+            mysqli_query($conn, $sql);
+        }
+    }
+}
+header("location:".$local);
+mysqli_close($conn);
+?>
