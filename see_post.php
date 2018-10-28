341d3a66792dc471334d6d426856e0180da81cdc
raymag <prayk11@outlook.com>
2018-10-28 17:47:26 -0300

    FEATURE: see_post - Atualizada

--- see_post.php
+++ see_post.php
@@ -24,6 +24,14 @@ if($query = mysqli_query($conn, $sql)){
     header("location:home.php");
 }
 mysqli_close($conn);
+$conn = connect();
+$sql = "SELECT COUNT(*) as qnt FROM notifications WHERE user = $id_user";
+if($q = mysqli_query($conn, $sql)){
+  $notifications = mysqli_fetch_assoc($q)["qnt"];
+}else{
+  $notifications = 0;
+}
+mysqli_close($conn);
 ?>
 <!DOCTYPE html>
 <html lang="PT-BR">
@@ -57,6 +65,14 @@ mysqli_close($conn);
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
       <li>
+      <?php
+        if($notifications>0){
+          echo '<a href="notifications.php" class="btn btn-warning" id="dark-text-nav">'.$notifications
+          .' <span class="glyphicon glyphicon-globe"></span></a>';
+        }else{
+          echo '<a href="notifications.php" class="btn btn-default"><span class="glyphicon glyphicon-globe"></span></a>';
+        }
+        ?></li><li>
     <form class="navbar-form navbar-right" onsubmit="return false">
       <div class="form-group">
         <input type="text" id="nav-search-input" class="form-control" placeholder="Pesquisar" value="<?php if(isset($_GET["q"])){echo $_GET["q"];} ?>">
@@ -69,12 +85,12 @@ mysqli_close($conn);
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" 
           role="button" aria-haspopup="true" aria-expanded="false">Mais <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Ver Comunidades</a></li>
+            <li><a href="see_all_communities.php">Ver Comunidades</a></li>
             <li><a href="create_community.php">Nova Comunidade</a></li>
             <li><a href="profile.php">Meu Perfil</a></li>
-            <li><a href="#">Seguidores</a></li>
+            <!-- <li><a href="#">Seguidores</a></li> -->
             <li role="separator" class="divider"></li>
-            <li><a href="#">Configurações</a></li>
+            <!-- <li><a href="#">Configurações</a></li> -->
             <li><a href="logout.php">Sair</a></li>
           </ul>
         </li>
@@ -110,7 +126,13 @@ mysqli_close($conn);
                             $conn = connect();
                             $sql = "INSERT INTO comments (user, post, comment_text) 
                             VALUES ($id_user, $id_post, '$txt')";
-                            mysqli_query($conn, $sql);
+                            if(mysqli_query($conn, $sql)){
+                                if($post["id_user"]!=$id_user){
+                                    $sql = "INSERT INTO notifications (user, post, community, type, acting_user)
+                                    VALUES (".$post["id_user"].",$id_post,".$post["community"].",'comment',$id_user)";
+                                    mysqli_query($conn, $sql);
+                                }
+                            }
                             mysqli_close($conn);
                         }
                         $conn = connect();
