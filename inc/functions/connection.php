b4d7c8d0aff982e674af779168bf9dc96c2ed70d
raymag <prayk11@outlook.com>
2018-11-02 15:06:21 -0300

    DB atualizado para o umbler

--- inc/functions/connection.php
+++ inc/functions/connection.php
@@ -1,9 +1,9 @@
 <?php
 function connect(){
-    $host = "localhost";
-    $user = "root";
-    $passwd = "root";
-    $db = "bond";
+    $host = "mysql995.umbler.com";
+    $user = "raymag";
+    $passwd = "xxxxxxxxxx";
+    $db = "magno_bond";
     $conn = mysqli_connect($host, $user, $passwd, $db);
     mysqli_set_charset($conn, "UTF-8");
     return $conn;
