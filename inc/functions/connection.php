913343131f8a4970400a02a6debb6df5c360c28a
raymag <prayk11@outlook.com>
2018-11-02 21:54:16 -0300

    connection att

--- inc/functions/connection.php
+++ inc/functions/connection.php
@@ -1,9 +1,9 @@
 <?php
 function connect(){
-    $host = "mysql995.umbler.com";
-    $user = "raymag";
-    $passwd = "xxxxxxxxxx";
-    $db = "magno_bond";
+    $host = "localhost";
+    $user = "xxxxxx";
+    $passwd = "xxxxxx";
+    $db = "xxxxxx";
     $conn = mysqli_connect($host, $user, $passwd, $db);
     mysqli_set_charset($conn, "UTF-8");
     return $conn;
